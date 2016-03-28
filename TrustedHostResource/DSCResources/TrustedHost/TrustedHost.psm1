d3f30d1196da83c0cf9db18b3471ea5118e4bbed
Dalmiro <dalmiro.granias@gmail.com>
2016-03-28 01:05:09 -0300

    First commit

---
+++ TrustedHostResource/DSCResources/TrustedHost/TrustedHost.psm1
@@ -0,0 +1,139 @@
+Function Get-TargetResource
+{
+    # TODO: Add parameters here
+    # Make sure to use the same parameters for
+    # Get-TargetResource, Set-TargetResource, and Test-TargetResource
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    param
+    (
+        #VM Name or IP
+        [parameter(Mandatory)]
+        [String]$Name
+    )
+    $trustedHost = Get-TrustedHost | ?{$_ -ieq $Name}
+
+    $returnValue = @{
+        Name              = $switch.Name        
+        Ensure            = if($trustedHost){'Present'}else{'Absent'}        
+    }
+
+    return $returnValue
+}
+
+Function Set-TargetResource
+{
+    # TODO:
+    # Make sure to use the same parameters for
+    # Get-TargetResource, Set-TargetResource, and Test-TargetResource
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    param
+    (
+        #VM Name or IP
+        [parameter(Mandatory)]
+        [String]$Name,
+        [ValidateSet("Present","Absent")]
+        [String]$Ensure = "Present"        
+    )
+
+    $allhosts = Get-TrustedHost
+    if($Ensure -eq 'Present'){
+        If($Name -iin $allhosts){
+            write-verbose "Host $Name is in the list as expected"
+            return $true
+        }
+        else{
+            write-verbose "Host $Name is not in the list, but it was expected to"
+            Add-TrustedHost -name $Name
+        }
+    }
+    Else{
+        If($Name -iin $allhosts){
+            write-verbose "Host $Name is in the list, but it wasn't supposed to"
+            Remove-TrustedHost -name $Name
+        }
+        else{
+            write-verbose  "Host $Name is not in the list as expected"
+            $true
+        }        
+    }
+}
+
+Function Test-TargetResource
+{
+    # TODO: Add parameters here
+    # Make sure to use the same parameters for
+    # Get-TargetResource, Set-TargetResource, and Test-TargetResource
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    param
+    (
+        #VM Name or IP
+        [parameter(Mandatory)]
+        [String]$Name,
+        [ValidateSet("Present","Absent")]
+        [String]$Ensure = "Present"
+    )
+
+    Write-Verbose -Message "Checking if Trusted Host $Name is $Ensure ..."
+    
+    #Getting all trusted hosts
+    $allhosts = Get-TrustedHost
+
+    If($Name -iin $allhosts){
+        # If trusted host should be there, and it is, return $true
+        If($Ensure -eq 'Present'){
+            return $true
+        }
+        # If trusted host should be absent, but is there, return $false
+        else{
+            return $false
+        }
+    }
+    else{
+        # If trusted host should be there, and its not, return $false
+        If($Ensure -eq 'Present'){
+            return $false
+        }
+        # If trusted host should be absent, and it is, return $true
+        else{
+            return $true
+        }
+    }
+
+}
+
+#region helper functions
+function destringifyhosts ([string]$hosts){    
+    return ($hosts.split(","))
+}
+
+function stringifyhosts ([string[]]$hosts){
+    $finalstring = ""
+
+    foreach ($h in $hosts){
+        $finalstring += ",$h"
+    }
+
+    return $finalstring.Remove(0,1)
+}
+
+function Get-TrustedHost{    
+    return (destringifyhosts (ls WSMan:\localhost\Client\TrustedHosts | select -ExpandProperty Value))
+}
+
+function Add-TrustedHost($name){
+    [System.Collections.Generic.List[System.String]]$allhosts = destringifyhosts (ls WSMan:\localhost\Client\TrustedHosts | select -ExpandProperty Value)
+    $allhosts.Add($name)
+    Set-Item WSMan:\localhost\client\trustedhosts -value (stringifyhosts $allhosts) -force
+}
+
+function Remove-TrustedHost ($name){
+    [System.Collections.Generic.List[System.String]]$allhosts = destringifyhosts (ls WSMan:\localhost\Client\TrustedHosts | select -ExpandProperty Value)
+    $allhosts.remove($name)
+    Set-Item WSMan:\localhost\client\trustedhosts -value (stringifyhosts $allhosts) -force
+}
+#endregion
+
+Export-ModuleMember -Function *-TargetResource
---
+++ TrustedHostResource/TrustedHostResource.psd1
@@ -0,0 +1,46 @@
+@{
+# Version number of this module.
+ModuleVersion = '1.0.0.0'
+
+# ID used to uniquely identify this module
+GUID = 'e2701f19-9fa5-473c-b958-8bb314b46d53'
+
+# Author of this module
+Author = 'Dalmiro Granas'
+
+# Description of the functionality provided by this module
+Description = 'Module with DSC Resource to set Trusted hosts on a computer to be able to remote into them using Powershell'
+
+# Minimum version of the Windows PowerShell engine required by this module
+PowerShellVersion = '4.0'
+
+# Minimum version of the common language runtime (CLR) required by this module
+CLRVersion = '4.0'
+
+# Functions to export from this module
+FunctionsToExport = '*'
+
+# Cmdlets to export from this module
+CmdletsToExport = '*'
+
+# Private data to pass to the module specified in RootModule/ModuleToProcess. This may also contain a PSData hashtable with additional module metadata used by PowerShell.
+PrivateData = @{
+
+    PSData = @{
+
+        # Tags applied to this module. These help with module discovery in online galleries.
+        Tags = @('DSC Resource', 'DSC', 'Trusted Hosts')
+
+        # A URL to the main website for this project.
+        ProjectUri = 'https://github.com/Dalmirog/TrustedHostResource'
+
+        # A URL to an icon representing this module.
+        # IconUri = ''
+
+        # ReleaseNotes of this module
+        # ReleaseNotes = ''
+
+    } # End of PSData hashtable
+
+} # End of PrivateData hashtable
+}
